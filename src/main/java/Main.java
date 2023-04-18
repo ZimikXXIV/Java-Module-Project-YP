d895c496bfde43f137a75cbf0dd13522a57db497
Zimik <Zimik24@yandex.ru>
2023-04-18 12:25:29 +0500

    1шаг

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,34 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int val;
+        val = getPeopleCount();
+    }
+
+    public static int getPeopleCount(){
+
+        Scanner scanner = new Scanner(System.in);
+        int getValue;
+        while (true)
+        {
+            System.out.println("Введите количество человек");
+            try {
+                getValue = scanner.nextInt();
+                if (getValue <= 1){
+
+                    System.out.println("Введено некоректное значение, значение людей должно быть больше 1");
+
+                }
+                else return getValue;
+            }
+            catch(Exception e) {
+                System.out.println("Введено некоректное значение, значение людей должно быть больше 1");
+                scanner.nextLine(); //высвобождает значение в считывании
+            }
+
+
+
+        }
+
     }
 }
